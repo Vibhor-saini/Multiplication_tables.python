e599f822fd43502db0bccbe21a388d425e9234cd
Vibhor saini <98160883+Vibhor-saini@users.noreply.github.com>
2022-01-22 22:28:49 -0800

    Add files via upload

---
+++ pset_8_03_multiplication_table.py
@@ -0,0 +1,5 @@
+num = int(input("Enter the number : = " ))
+print(f"Multiplication table of {num} is :")
+for i in range(1 , 11):
+# print(str(num) + "x" + str(i) + "=" + str(num*i))
+ print(f"{num} X  {i} = {num*i}")
